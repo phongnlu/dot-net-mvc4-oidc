4745442eb733003b511010adccaa8a061849ae6e
phong_lu <phong_lu@symantec.com>
2018-09-12 00:45:45 -0700

    initial commit

---
+++ MVC4 OIDC/MVC4 OIDC/App_Start/Startup.Auth.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+using System.Web.Helpers;
+using System.Security.Claims;
+
+namespace MVC4_OIDC
+{
+    public partial class Startup
+    {
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/LogIn")
+            });
+
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            var googleClientId = "835072215661-etftrpjnap9bp5ouu6bu58h6qa7ifg84.apps.googleusercontent.com";
+            var googleClientSecret = "KGZgMjMTUihLLLgsLzNA2PH2";
+            app.UseGoogleAuthentication(clientId: googleClientId, clientSecret: googleClientSecret);
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+        }
+    }
+}
---
+++ MVC4 OIDC/MVC4 OIDC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC4_OIDC.Startup))]
+namespace MVC4_OIDC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
